55e397a65cfe7a56eb0ac694219bef5c4835814a
PabloWojat <pablowojat@gmail.com>
2023-09-27 21:16:20 -0300

    Se agrega el proyecto

---
+++ config.h
@@ -0,0 +1,11 @@
+const char* ssid     = "Claro2022";
+const char* password = "12341234";
+const char* hostname = "ESP8266_Nebula";
+
+IPAddress ip(192, 168, 1, 200);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+
+
+String clase1;
+String clase2;
